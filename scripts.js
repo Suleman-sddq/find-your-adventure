8ef3138156d693f70c83c7cc50aca9355d8e1e61
M.Suleman <sulemansddq@gmail.com>
2022-10-06 12:24:12 +0500

    fixed background blur

--- scripts.js
+++ scripts.js
@@ -4,23 +4,18 @@ function navSlide() {
   burger.addEventListener("click", function () {
     if (navLinks.classList.contains("nav-active")) {
       navLinks.classList.remove("nav-active");
-      document.querySelector(".hero").style.filter = "none";
-      // document.querySelector(".logo").style.filter = "none";
     } else {
       navLinks.classList.add("nav-active");
-      document.querySelector(".hero").style.filter = "blur(5px)";
-      // document.querySelector(".logo").style.filter = "blur(5px)";
+
     }
   });
   navLinks.addEventListener("click", () => {
     if (navLinks.classList.contains("nav-active")) {
       navLinks.classList.remove("nav-active");
-      document.querySelector(".hero").style.filter = "none";
-      // document.querySelector(".logo").style.filter = "none";
+
     } else {
       navLinks.classList.add("nav-active");
-      document.querySelector(".hero").style.filter = "blur(5px)";
-      // document.querySelector(".logo").style.filter = "blur(5px)";
+
     }
   })
 }
