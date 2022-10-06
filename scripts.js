56559f33c91a87fe0c78c35ae1fb7256371141d9
M.Suleman <sulemansddq@gmail.com>
2022-10-06 11:20:56 +0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const navLinks = document.querySelector(".nav-links");
+  burger.addEventListener("click", function() {
+    if (navLinks.classList.contains("nav-active")) {
+      navLinks.classList.remove("nav-active");
+      document.querySelector(".hero").style.filter = "none";
+      document.querySelector(".logo").style.filter = "none";
+    } else {
+      navLinks.classList.add("nav-active");
+      document.querySelector(".hero").style.filter = "blur(5px)";
+      document.querySelector(".logo").style.filter = "blur(5px)";
+    }
+  });
+}
+navSlide();
