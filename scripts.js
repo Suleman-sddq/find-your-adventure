5d685409c9815996c24f4c8e8af81745b1256d9c
M.Suleman <sulemansddq@gmail.com>
2022-10-06 12:19:05 +0500

    fixed mobile screen sliding menu

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 function navSlide() {
   const burger = document.querySelector(".burger");
   const navLinks = document.querySelector(".nav-links");
-  burger.addEventListener("click", function() {
+  burger.addEventListener("click", function () {
     if (navLinks.classList.contains("nav-active")) {
       navLinks.classList.remove("nav-active");
       document.querySelector(".hero").style.filter = "none";
-      document.querySelector(".logo").style.filter = "none";
+      // document.querySelector(".logo").style.filter = "none";
     } else {
       navLinks.classList.add("nav-active");
       document.querySelector(".hero").style.filter = "blur(5px)";
-      document.querySelector(".logo").style.filter = "blur(5px)";
+      // document.querySelector(".logo").style.filter = "blur(5px)";
     }
   });
+  navLinks.addEventListener("click", () => {
+    if (navLinks.classList.contains("nav-active")) {
+      navLinks.classList.remove("nav-active");
+      document.querySelector(".hero").style.filter = "none";
+      // document.querySelector(".logo").style.filter = "none";
+    } else {
+      navLinks.classList.add("nav-active");
+      document.querySelector(".hero").style.filter = "blur(5px)";
+      // document.querySelector(".logo").style.filter = "blur(5px)";
+    }
+  })
 }
 navSlide();
